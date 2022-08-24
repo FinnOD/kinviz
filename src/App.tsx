d6a96ecf067ac4506907c935ca42ee9900890c18
finnod <finnodonoghue@gmail.com>
2022-08-24 20:48:40 +1000

    initial commit. base app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import ForceGraph3D from "react-force-graph-3d";
 import './App.css';
+
+const networkJSON = require("./data/networkKinasesSmall.json");
+// const networkJSON = require("./data/example2.json");
+
+const DynamicGraph = () => {
+  const [data, setData] = useState(networkJSON);
+  
+  return <ForceGraph3D
+    graphData={data}
+    linkCurvature={0.4}
+  />;
+};
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="main">
+        <div className="maingraph">
+          <DynamicGraph />
+        </div>
+      </div>
     </div>
   );
 }
